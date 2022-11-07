8c7591a867cceeb235f2fcb384f4c0be33272766
= <monique.rimbert@etu.univ-nantes.fr>
2022-11-07 16:09:57 +0100

    Creation du package Quest

---
+++ src/Quest/QuestKillMonster.java
@@ -0,0 +1,12 @@
+package Quest;
+
+/**
+ * QuestKillMonster.java
+ * QuestKillMonster décrit une quête où l'on doit tuer un monstre
+ * @author Monique RIMBERT
+ * @author Wiame TAII
+ */
+
+public class QuestKillMonster {
+    
+}
