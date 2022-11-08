3ffc181fcb22b9d6af6ab69d5ad51b665760769e
= <monique.rimbert@etu.univ-nantes.fr>
2022-11-08 14:26:16 +0100

    Ecriture de la classe abstraite Quest

--- src/Quest/Quest.java
+++ src/Quest/Quest.java
@@ -1,5 +1,5 @@
 package Quest;
-
+import Item;
 /**
  * Quest.java
  * Quest décrit une quête que ce soit tuer un monstre ou recolter un item
@@ -7,5 +7,20 @@ package Quest;
  */
 
 public abstract class Quest {
-    //TODO
+    private String title;
+    private boolean is_accomplished;
+    private Item reward;
+    private int bonus_exp;
+
+    public Quest(String title, Item reward, int bonus_exp) {
+        this.title = title;
+        this.is_accomplished = false;
+        this.reward = reward;
+        this.bonus_exp = bonus_exp;
+    }
+
+    /**
+     * Marque une quete comme accomplie
+     */
+    public abstract void submit();
 }
