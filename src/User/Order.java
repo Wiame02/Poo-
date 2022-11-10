0d3212138071e19988e18303fda56750c578d285
Katel Hignard <E214928S>
2022-11-10 21:08:49 +0100

    Ajout de toutes les constantes, abilités à définir pour chaque classe

--- src/User/Order.java
+++ src/User/Order.java
@@ -1,4 +1,5 @@
 package User;
+import Entity.*;
 
 /**
  * Order.java
@@ -11,18 +12,57 @@ public enum Order {
     COMPUTER_SCIENTIST(){
         
         @Override
-        public void useSpecialAbility(){
+        /**
+         * L'informaticien peut changer l'entité qui se trouve dans la même zone que lui aléatoirement
+         * @param e
+        */
+        public void useSpecialAbility(Entity e){
+            //TODO
+        }
+    },
+    MATHEMATICIAN(){
+
+        @Override
+        /**
+         * Le mathematicien peut 
+         */
+        public void useSpecialAbility(Entity e){
+            //TODO
+        }
+    },
+    PHYSICIST(){
+
+        @Override
+        /**
+         * Le physicien peut
+         */
+        public void useSpecialAbility(Entity e){
+            //TODO
+        }
+    },
+    CHEMIST(){
+
+        @Override
+        /**
+         * Le chimiste peut
+         */
+        public void useSpecialAbility(Entity e){
+            //TODO
+        }
+    },
+    BIOLOGIST(){
+
+        @Override
+        /**
+         * Le biologist peut
+         */
+        public void useSpecialAbility(Entity e){
             //TODO
         }
     };
-/* Attribus */
 
-/* Constructeurs */
-
-/* Méthodes */
-
-    /**
-     * Permet d'utiliser l'abilité spécifique à une classe (Order)
-     */
-    public abstract void useSpecialAbility();
+/**
+ * Permet d'utiliser l'abilité spécifique à une classe (Order)
+ */
+    public abstract void useSpecialAbility(Entity e);
 }
