355b61bcbe1814ef947fa430ca47c750b13d72d9
Monique <monique.rimbert@etu.univ-nantes.fr>
2022-11-14 14:58:07 +0100

    Creation d'une classe d'exceptions liées à l'application

---
+++ src/Application/ApplicationException.java
@@ -0,0 +1,16 @@
+/**
+ * Classe qui gère les exceptions spécifiques au package Application
+ * @author Monique RIMBERT
+ */
+
+
+class ApplicationException extends Exception {
+    
+    /**
+     * Creation d'une exception de type GameException
+     * @param message Message de l'erreur
+     */
+    public ApplicationException(String message) {
+        super(message);
+    }
+}
