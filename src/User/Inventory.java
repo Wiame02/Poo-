872264e7c5698a0fac5f6ddf125054f1aea2e9bd
Katel Hignard <E214928S>
2022-11-15 15:18:51 +0100

    Création de la classe pour un Inventaire avec ses methodes nécéssaires

---
+++ src/User/Inventory.java
@@ -0,0 +1,53 @@
+package User;
+import Item.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Inventory {
+    private ArrayList<Item> items;
+
+    /**
+     * Constructeur d'un Inventaire
+     */
+    Inventory(){
+        this.items = new ArrayList<Item>();
+    }
+
+    /**
+     * Ajoute une item à l'inventaire
+     * @param e un item
+     */
+    public void add_item(Item e){
+        // Determiner si l'item existe deja dans l'inventaire (WHILE)
+            // Si oui ajouter 1 au nombre d'exemplaire
+            // Si non ajouter l'item et mettre le nb exemplaire à 1
+    }
+
+    /**
+     * Supprime l'item selon son indice
+     * @param id l'indice du l'item a supprimer
+     */
+    public void delete_item(int id){
+        // Trouver l'item dans l'inventaire (WHILE)
+        // Supprimer l'item (en ignorant le nb exemplaire)
+    }
+
+    /**
+     * Utilise un exemplaire d'un item donné par son nom
+     * @param name nom de l'item à utiliser
+     */
+    public void use_item(String name){
+        // id = Trouver l'item (WHILE)
+        // enlever 1 exemplaire
+        // VERIF : Si 0 exemplaire -> delete_item(id)
+    }
+
+    /**
+     * Affiche l'inventaire entier
+     */
+    public void display_inventory(){
+        for(Item item : this.items){
+            // Afficher nom de l'item
+        }
+    }
+}
