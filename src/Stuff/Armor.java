381162faa7b7fe79bbb5b79edeed79044d21ee12
Katel Hignard <E214928S>
2022-11-15 19:08:42 +0100

    Correction du problème de package : la syntaxe n'était pas correcte, et "item" non reconnu comme package ce qui entraine un changement de nom

---
+++ src/Stuff/Armor.java
@@ -0,0 +1,19 @@
+package Stuff;
+//item.java 
+// Item les objets utiliser par le personnage 
+// @author Taii wiame 
+
+public class Armor extends Item {
+    private int defense_point ;
+
+//--------------------constructeur ---------------------------
+public Armor(String name , int durability , int defense_point){
+  super (name , durability ) ;
+  this.defense_point =defense_point;
+} 
+
+//---------------------methode--------------------------------
+public int getdefense_point()    {return this.defense_point} 
+@Override  
+public void use_damage_n (int nb_damage) {this.use_damage (nb_damage) ;} 
+}
