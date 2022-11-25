8f2f7151169db5997e0272d2684e2087fa06ba96
Monique <monique.rimbert@etu.univ-nantes.fr>
2022-11-25 14:59:59 +0100

    Ajout de fonctions

--- src/Localization/Board.java
+++ src/Localization/Board.java
@@ -21,6 +21,7 @@ public class Board {
      * GETTERS
      */
     public World get_world_at(int i) {return this.worlds.get(i);};
+    public ArrayList<World> get_worlds() {return this.worlds;};
 
     /**
      * Ajout d'un nouveau monde sur notre plateau
@@ -28,5 +29,13 @@ public class Board {
      */
     public void add_world(World w) {
         this.worlds.add(w);
+    }
+    
+    /**
+     * Ajout de plusieurs nouveaux mondes sur notre plateau
+     * @param l_w
+     */
+    public void add_all_worlds(ArrayList<World> l_w) {
+        this.worlds.addAll(l_w);
     }
 }
