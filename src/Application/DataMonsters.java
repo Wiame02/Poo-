3f582718a54f64b0ce033eff9e294e3ece67b191
Katel Hignard <E214928S>
2022-11-27 16:59:30 +0100

    Données des monstres crées

--- src/Application/DataMonsters.java
+++ src/Application/DataMonsters.java
@@ -1,5 +1,5 @@
 package Application;
-import Entity.Monster;
+import Entity.*;
 import java.util.ArrayList;
 
 /**
@@ -12,7 +12,11 @@ public class DataMonsters {
     public static ArrayList<Monster> create_monsters_valkiem(){
         ArrayList<Monster> monsters = new ArrayList<Monster>();
         
-        //TODO : Ajouter les monstres
+        monsters.add(new Monster("Mammouth Agé",150,Species.MAMMOTH,22));
+        monsters.add(new Monster("Bébé Mammouth",25,Species.MAMMOTH,3));
+        monsters.add(new Monster("Griffon ancestral",75,Species.GRIFFIN,10));
+        monsters.add(new Monster("Molosse enragé",40,Species.MASTIFF,5));
+        monsters.add(new Monster("Golem terreux",250,Species.EARTH_ELEMENTAL,45));
 
         return monsters;
     }
@@ -20,7 +24,13 @@ public class DataMonsters {
     public static ArrayList<Monster> create_monsters_cimebel(){
         ArrayList<Monster> monsters = new ArrayList<Monster>();
 
-        //TODO : Ajouter les monstres
+        monsters.add(new Monster("Zombie décomposé",25,Species.ZOMBIE,12));
+        monsters.add(new Monster("Zombie",30,Species.ZOMBIE,25));
+        monsters.add(new Monster("Mort-vivant 1",45,Species.UNDEAD,20));
+        monsters.add(new Monster("Mort-vivant 2",20,Species.UNDEAD,3));
+        monsters.add(new Monster("Goule",110,Species.GHOUL,30));
+        monsters.add(new Monster("Algoule",40,Species.ALGOULE,5));
+        monsters.add(new Monster("Djin mytérieux",80,Species.DJIN,45));
 
         return monsters;
     }
@@ -28,7 +38,12 @@ public class DataMonsters {
     public static ArrayList<Monster> create_monsters_codix(){
         ArrayList<Monster> monsters = new ArrayList<Monster>();
 
-        //TODO : Ajouter les monstres
+        monsters.add(new Monster("Drone Défaillant",15, Species.DRONE, 30));
+        monsters.add(new Monster("Drone Corrompu",20, Species.DRONE, 45));
+        monsters.add(new Monster("Cyborg Défaillant", 150, Species.CYBORG, 75));
+        monsters.add(new Monster("Cyborg corrompu", 230, Species.CYBORG, 120));
+        monsters.add(new Monster("Luchi", 500, Species.DEMON, 175));
+        monsters.add(new Monster("Dragon de Luchi", 430, Species.DEMONIC_DRAGON, 260));
 
         return monsters;
     }
