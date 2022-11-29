b44e9f9c1bc4a44e6e793090ceba13d902d9f044
wiame <wiamtaii13@gmail.com>
2022-11-29 08:13:44 +0100

    use() de Item

--- src/Stuff/Armor.java
+++ src/Stuff/Armor.java
@@ -1,46 +1,60 @@
 package Stuff;
 //item.java 
+
 // Item les objets utiliser par le personnage 
 // @author Taii wiame 
 
 public class Armor extends Item {
-    private int defense_point ;
+    private int defense_point;
     private Type type;
     private int durability;
 
-     /**
+    /**
      * Constructeur de Armor
      */
-    public Armor(String name , int durability , int defense_point, Type type){
-        super (name) ;
+    public Armor(String name, int durability, int defense_point, Type type) {
+        super(name);
         this.durability = durability;
-        this.defense_point =defense_point;
+        this.defense_point = defense_point;
         this.type = type;
-    } 
+    }
 
-    public int get_durability() { return this.durability; }
-    public Type get_type(){ return this.type;}
+    public int get_durability() {
+        return this.durability;
+    }
 
-    /**
-    * SETTERS
-    */
-    public void set_defense_point(int defense_point) {this.defense_point = defense_point;} 
-    public void set_durability(int durability){ this.durability = durability; }    
-
+    public Type get_type() {
+        return this.type;
+    }
 
     /**
-    * METHODE
-    */
-    public int getdefense_point()    {return this.defense_point;} 
-  
-    public void use_damage_n (int nb_damage) {this.use_damage (nb_damage) ;} 
+     * SETTERS
+     */
+    public void set_defense_point(int defense_point) {
+        this.defense_point = defense_point;
+    }
+
+    public void set_durability(int durability) {
+        this.durability = durability;
+    }
 
     /**
-      * affichage de l'item
-      */
+     * METHODE
+     */
+    public int getdefense_point() {
+        return this.defense_point;
+    }
+
+    public void use_damage_n(int nb_damage) {
+        this.use_damage_n(nb_damage);
+    }
+
+    /**
+     * affichage de l'item
+     */
     @Override
-    public String to_string(){
-        return super.to_string()+"("+this.defense_point+" ,"+this.type+ ", "+this.durability+" )";
+    public String to_string() {
+        return super.to_string() + "(" + this.defense_point + " ," + this.type + ", " + this.durability + " )";
     }
 
 }
--- src/Stuff/Item.java
+++ src/Stuff/Item.java
@@ -1,66 +1,67 @@
 package Stuff;
 //item.java 
+
 // Item les objets utiliser par le personnage 
 // @author Taii wiame 
 
-
 public class Item {
-    private String name;    
+    private String name;
     private int quantity;
-
 
     /**
      * Constructeur de Item
      */
-    public Item (String name) {
+    public Item(String name) {
         this.name = name;
         this.quantity = 1;
-    }  
+    }
 
     /**
      * GETTERS
      */
 
-    public String get_name()  {return this.name;}
-    public int get_quantity() {return this.quantity;}
+    public String get_name() {
+        return this.name;
+    }
 
-     /**
+    public int get_quantity() {
+        return this.quantity;
+    }
+
+    /**
      * SETTERS
      */
-    public void set_name (String name) {this.name = name;} 
-    public void set_quantity (int quantity) {this.quantity = 1;}
+    public void set_name(String name) {
+        this.name = name;
+    }
 
-    
+    public void set_quantity(int quantity) {
+        this.quantity = 1;
+    }
 
     /**
      * affichage de l'item
      */
-    public String to_string(){
+    public String to_string() {
         return this.name;
     }
 
     /**
-     * verification si un item est egale à cette item la 
+     * verification si un item est egale à cette item la
+     * 
      * @param i
      */
-    public boolean is_equal(Item i){
-        return (i.get_name()==this.name);
+    public boolean is_equal(Item i) {
+        return (i.get_name() == this.name);
     }
 
     /**
-     * vérifie la diminution la quantité d'un  Item de 1
+     * vérifie la diminution la quantité d'un Item de 1
+     * 
      * @param i
      */
-    public int use (Item i) { 
-        return (i.get_quantity()-= 1);
+    public int use(Item i) {
+        int res = i.get_quantity() - 1;
+        return res;
     }
 }
- 
- 
-
-
-
-
-
-
-    
