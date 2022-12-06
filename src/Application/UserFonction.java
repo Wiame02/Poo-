6d7796e4a7a4a2e097f2275f6c33d797ffeb0fdb
= <monique.rimbert@etu.univ-nantes.fr>
2022-12-06 12:32:05 +0100

    Déplacement

--- src/Application/UserFonction.java
+++ src/Application/UserFonction.java
@@ -1,8 +1,23 @@
 package Application;
+import java.util.ArrayList;
 import Entity.*;
 import User.*;
+import Localization.*;
 
 public class UserFonction {
+    /**
+     * @see User.Player#moveToLinkedArea(Area)
+     * @param p
+     * @param idArea
+     */
+    public static void moveToLinkedArea(Player p, int idArea) {
+        ArrayList<Area> areas =  p.getCurrentArea().getAccessAreas();
+
+        if (0<=idArea && idArea<areas.size()) {
+            p.moveTo(p.getCurrentArea().getAccessAreas().get(idArea));
+        }
+        System.out.println(p.getCurrentArea() + "\n");
+    }
     
     public static void fight(Player p,Monster m){
         while(p.isAlive() && m.isAlive()){
